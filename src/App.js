17cd6f418a3bac9d21de7fb3d8899a04d130d506
Rokas <rookas.rudzenskas@gmail.com>
2021-06-09 09:58:26 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Header from "./components/Header";
-
+import React from "react"
 function App() {
   return (
     <div className="App">
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,47 @@
 import React from 'react';
 import "./styles/Header.css";
+import SearchIcon from '@material-ui/icons/Search'
+import HomeIcon from '@material-ui/icons/Home'
+import FlagIcon from '@material-ui/icons/Flag'
+import SubscriptionsOutlinedIcon from '@material-ui/icons/SubscriptionsOutlined'
+import StorefrontOutlinedIcon from '@material-ui/icons/StorefrontOutlined'
+import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle'
+import { Avatar, IconButton } from '@material-ui/core'
+import AddIcon from '@material-ui/icons/Add'
+import ForumIcon from '@material-ui/icons/Forum'
+import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 
 const Header = () => {
     return (
         <div className="header">
-            
+            <div className="header__left">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg" alt=""/>
+            </div>
+
+            <div className="header__input">
+                <SearchIcon />
+                <input type="text" placeholder="Search Facebook"/>
+            </div>
+
+            <div className="header__center">
+                <div className="header__option header__option--active">
+                    <HomeIcon fontSize='large' />
+                </div>
+                <div className="header__option">
+                    <FlagIcon fontSize='large' />
+                </div>
+                <div className="header__option">
+                    <SubscriptionsOutlinedIcon fontSize='large' />
+                </div>
+                <div className="header__option">
+                    <StorefrontOutlinedIcon fontSize='large' />
+                </div>
+                <div className="header__option">
+                    <SupervisedUserCircleIcon fontSize='large' />
+                </div>
+            </div>S
+
         </div>
     );
 };
