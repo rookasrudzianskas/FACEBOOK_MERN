a080dcbcf266c98489d8b6da5fdc4ffb68e9a728
Rokas <rookas.rudzenskas@gmail.com>
2021-06-10 12:24:53 +0300

    Rokas

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,22 +1,48 @@
-import React from 'react';
-import "./styles/Post.css";
-import {Avatar} from "@material-ui/core";
+import { Avatar } from '@material-ui/core';
+import React, { useState } from 'react';
+import './styles/Post.css';
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
+import NearMeIcon from '@material-ui/icons/NearMe';
+import ExpandMoreOutlined from '@material-ui/icons/ExpandMoreOutlined';
+import AccountCircleIclon from '@material-ui/icons/AccountCircle';
 
-const Post = ({profilePic, timestamp, message, username, imgName}) => {
+const Post = ({ profilePic, imgName, username, timestamp, message }) => {
     return (
-        <div className="post">
+        <div className='post' >
             <div className="post__top">
-                <Avatar src={profilePic} className="post__avatar" />
-
+                <Avatar src={profilePic} className='post__avatar' />
                 <div className="post__topInfo">
                     <h3>{username}</h3>
                     <p>{new Date(parseInt(timestamp)).toUTCString()}</p>
+                </div>
+            </div>
+            <div className="post__bottom">
+                <p>{message}</p>
+            </div>
 
 
+
+            <div className="post__options">
+                <div className="post__option">
+                    <ThumbUpIcon />
+                    <p>Like</p>
+                </div>
+                <div className="post__option">
+                    <ChatBubbleOutlineIcon />
+                    <p>Comment</p>
+                </div>
+                <div className="post__option">
+                    <NearMeIcon />
+                    <p>Share</p>
+                </div>
+                <div className="post__option">
+                    <AccountCircleIclon />
+                    <ExpandMoreOutlined />
                 </div>
             </div>
         </div>
-    );
-};
+    )
+}
 
 export default Post;
