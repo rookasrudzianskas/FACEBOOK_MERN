6016186e505fd6ae6a3ea96ad0f38febe04917d2
Rokas <rookas.rudzenskas@gmail.com>
2021-06-13 18:05:14 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ function App() {
         {/*  the stuff is coming on in here*/}
         {/*  the stuff is coming on in here*/}
         {/*  the stuff is coming on in here*/}
+        {/*  the stuff is coming on in here*/}
         <Feed />
           <Widget />
 
